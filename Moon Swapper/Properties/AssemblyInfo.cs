c0691ca47aca8d7a13c405c59ff4d39dbd2f860f
Storm <78948929+Mysa99@users.noreply.github.com>
2021-02-22 20:44:10 +0100

    Update AssemblyInfo.cs

--- Moon Swapper/Properties/AssemblyInfo.cs
+++ Moon Swapper/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Moon Swapper")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Moon Swapper")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("CREDIT MOON SWAPPER!")]
+[assembly: AssemblyDescription("CREDIT MOON SWAPPER!")]
+[assembly: AssemblyConfiguration("CREDIT MOON SWAPPER!")]
+[assembly: AssemblyCompany("CREDIT MOON SWAPPER!")]
+[assembly: AssemblyProduct("CREDIT MOON SWAPPER!")]
+[assembly: AssemblyCopyright("CREDIT MOON SWAPPER! ©  2021")]
+[assembly: AssemblyTrademark("CREDIT MOON SWAPPER!")]
+[assembly: AssemblyCulture("CREDIT MOON SWAPPER!")]
 
 // Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
 // für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
